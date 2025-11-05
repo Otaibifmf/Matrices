20a753cf94d82cf159c1508dca07a487448f2b60
Otaibifmf <Otaibifmf@gmail.com>
2025-11-05 22:23:28 +0300

    Learning Matrices through python. Addition function added

---
+++ ArithmeticOperations.py
@@ -0,0 +1,26 @@
+
+#3x3 Matrix
+A = [1, 4, 5,
+     2, 5, 6,
+     7, 2, 1]
+
+B = [8, 5, 3, 
+     9, 4, 2,
+     1, 8, 0]
+
+C = [0, 0, 0, 
+     0, 0, 0,
+     0, 0, 0]
+
+
+# Creates a function
+def Addition(X,Y):
+    
+    for i in range(9): # In range of 9 when Matrix is 3X3.
+        C[i] = A[i] + B[i]
+
+
+
+Addition(A,B)
+print(C)
+
