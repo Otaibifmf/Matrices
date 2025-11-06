06bcfacbdff11011dcd604285e300019456db734
sackboy <118112953+DrSackboy@users.noreply.github.com>
2025-11-06 13:44:52 +0300

    Only adds and subtracts same sized matrices

--- ArithmeticOperations.py
+++ ArithmeticOperations.py
@@ -16,23 +16,30 @@ B = [8, 5, 3,
      9, 4, 2,
      1, 8, 0]
 
-C = [0, 0, 0, 
-     0, 0, 0,
-     0, 0, 0]
+#2X2 Matrix
+D = [1, 4,
+     7, 9]
 
+E = [9, 2,
+     3, 5]
 
 # Creates a function
 def Addition(X,Y,Z):
-    
-     for i in range(size): 
-          Z[i] = X[i] + Y[i]
-     print(Z)
+    if(len(X) == len(Y) == len(Z)): # If size of all arrays are equal then proceed else quit
+        for i in range(size): 
+            Z[i] = X[i] + Y[i]
+        print(Z)
+    else:
+     print("Martices must be the same size")      
 
 
 def Subtraction(X,Y,Z):
+    if(len(X) == len(Y) == len(Z)):
+        for i in range(size): 
+            Z[i] = X[i] - Y[i]
+        print(Z)
+    else:
+     print("Martices must be the same size")     
 
-     for i in range(size):
-          Z[i] = X[i] - Y[i]
-     print(Z)
 
-
+Addition(D,E,U)
