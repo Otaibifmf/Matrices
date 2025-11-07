1ac4937a766928ceca4aad68cdf3315d711a3c17
sackboy <118112953+DrSackboy@users.noreply.github.com>
2025-11-07 14:07:34 +0300

    removed temp file

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-rows = int(input("How many rows? "))
-# cols = int(input("How many columns? "))
-# size = rows * cols
-# UserMatrix = [0] * size # Populates array sized by user with zeros. Size 9
-
-# TwoDUserMatrix = [[0] * cols for _ in range(rows)] # Size 3
