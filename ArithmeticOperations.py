34be756912c32935717add74ae420858ff740470
Otaibifmf <Otaibifmf@gmail.com>
2025-11-07 11:29:22 +0300

    Removed user based result matrix because its useless

--- ArithmeticOperations.py
+++ ArithmeticOperations.py
@@ -1,12 +1,3 @@
-# User Matrix
-
-# rows = int(input("How many rows? "))
-# cols = int(input("How many columns? "))
-# size = rows * cols
-# UserMatrix = [0] * size # Populates array sized by user with zeros. Size 9
-
-# TwoDUserMatrix = [[0] * cols for _ in range(rows)] # Size 3
-
 # 2D Array, 3x3 Matrix
 
 TwoDArray1 = [[1,2,3], # Size 3
@@ -40,18 +31,20 @@ E = [9, 2,
      3, 5]
 
 # Creates a function
-def Addition(X,Y,Z): # Takes three arrays
-    if(len(X) == len(Y) == len(Z)): # If size of all arrays are equal then proceed else quit
-        for i in range(size): 
-            Z[i] = X[i] + Y[i]
+def Addition(X,Y): # Takes two arrays
+    if(len(X) == len(Y)): # If size of both arrays are equal
+        Z = [0] * len(X) # Result array is same size of X, filled with zeros
+        for i in range(len(X)): 
+            Z[i] = X[i] + Y[i] 
         print(Z)
     else:
      print("Martices must be the same size")      
 
 
-def Subtraction(X,Y,Z):
-    if(len(X) == len(Y) == len(Z)):
-        for i in range(size): 
+def Subtraction(X,Y):
+    if(len(X) == len(Y)):
+        Z = [0] * len(X)
+        for i in range(len(X)): 
             Z[i] = X[i] - Y[i]
         print(Z)
     else:
@@ -80,4 +73,4 @@ def DotProduct(X,Y):
     print(Z)
 
 
-DotProduct(TwoDArray1,TwoDArray2)
+#DotProduct(TwoDArray1,TwoDArray2)
